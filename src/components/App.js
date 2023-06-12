3396d86536389628cd6f6d4a53c82abbe4a00697
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-06-12 12:55:31 +0530

    zz

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,53 @@
-import React, { useState} from "react";
-import '../styles/App.css';
+import React,{useState} from "react";
+import './../styles/App.css';
 
-
-let App=()=>{
-    let [data,setData]=useState("")
-    let [fistame,setFirstName]=useState("");
-    let [secondname,setSecondName]=useState("");
-    function addData(){
-        if(fistame==""|| secondname==""){
-            setData("Please Enter valid input")
-            return;
-        }
-        let arr1=fistame.split("");
-        let arr11=[];
-        let arr2=secondname.split("");
-        for(let i=0;i<arr1.length;i++){
-            let flag=false;
-            for(let j=0;j<arr2.length;j++){
-                if(arr1[i]==arr2[j]){
-                    arr2.splice(j,1);
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false){
-                arr11.push(arr1[i]);
-            }
-        }    
-        
-        let str=arr11.join("")+arr2.join("");
-        let n=(str.length)%6;
-        switch(n){
-            case 1:return setData('Fiends');
-            case 2:return setData('Love');
-            case 3:return setData('Affection');
-            case 4:return setData('Marriage');
-            case 5:return setData('Enemy');
-            case 0:return setData('Siblings');
-        }
-
-    }
-
-    function clear(){
-        setData("");
-        setFirstName("");
-        setSecondName("")
-    }  
-    return(<div id="main">
-        <input type="text" name="name1" data-testid="input1" onChange={(e)=>setFirstName(e.target.value)} placeholder="Enter first name" value={fistame}/>
-        <input type="text" name="name2" data-testid="input2" onChange={(e)=>setSecondName(e.target.value)} value={secondname} placeholder="Enter second name"/>
-        <button data-testid="calculate_relationship" onClick={addData}>Calculate Relationship Future</button>
-        <button data-testid="clear" onClick={clear}>Clear</button>
-        <h3 data-testid="answer">{data}</h3>
-    </div>)
+const App = () => {
+  let [gender, setGender] = useState("");
+  return (
+    <div>
+        <h2>Select your gender:</h2>
+        <input 
+        type="radio"
+        name="gender"
+        value="Male"
+        id="male"
+        onChange={(e) => setGender(e.target.value)}
+         />
+         <label forHTML = "male">Male</label>
+         <input 
+        type="radio"
+        name="gender"
+        value="Female"
+        id="female"
+        onChange={(e) => setGender(e.target.value)}
+         />
+         <label forHTML = "female">Female</label>
+         {
+          gender === "Male" &&
+          <div>
+            <h2>Select your shirt size:</h2>
+            <select>
+              <option value="se">Select Size</option>
+              <option value="small">Small</option>
+              <option value="med">Medium</option>
+              <option value="large">Large</option>
+            </select>
+          </div>
+         }
+         {
+          gender === "Female" &&
+          <div>
+            <h2>Select your dress size:</h2>
+            <select>
+              <option value="se">Select Size</option>
+              <option value="two">2</option>
+              <option value="four">4</option>
+              <option value="six">6</option>
+            </select>
+          </div>
+         }
+    </div>
+  )
 }
-
 
 export default App
